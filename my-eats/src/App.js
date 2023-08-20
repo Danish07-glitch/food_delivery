f2fc321a83b54d7eb45063e0a7b3c0d26e27dde6
Danish07-glitch <85664578+Danish07-glitch@users.noreply.github.com>
2023-08-20 23:42:28 +0530

    first

---
+++ my-eats/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
+import Foods from './components/Foods';
+
+function App() {
+  return (
+    
+    <>
+    <Navbar/>
+    <Hero/>
+    <HeadlineCards/>
+    <Foods/>
+    </>
+  );
+}
+
+export default App;
